b9d5f56e093118dd072a8a63d06fabfb0431ab78
Rodolfo Cuevas <rcuevaspantoja@gmail.com>
2023-11-03 18:57:04 -0300

    3-11-2023

---
+++ src/components/loadingspinner.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function loadingspinner() {
+  return (
+    <div className='loading-screen'>
+        <div className='spinner'> </div>
+    </div>
+  )
+}
+
+export default loadingspinner
